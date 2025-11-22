3695f6ca0ded5076d00e86c8fa4724eac3e74619
Nate <nate@8ball.space>
2025-11-22 16:15:45 -0500

    Updated website
    Signed-off-by: Nate <nate@8ball.space>

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// --- Middleware Setup ---
+app.use(express.json());
+
+// --- Serve Website ---
+app.use(express.static(path.join(__dirname)));
+
+// --- Server Start ---
+app.listen(PORT, () => {
+    console.log(`
+        Server is running on: http://localhost:${PORT}
+    `);
+});
